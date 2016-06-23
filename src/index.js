7ce9e29ac1d6f99d88e9450331406348fd5c556c
tomas.jilek <tomas.jilek@morosystems.cz>
2016-06-23 12:17:20 +0200

    jira header

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import {Router, useRouterHistory} from 'react-router';
 import routes from './routes';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.less'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import './styles/style.css'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import Immutable from "immutable";
